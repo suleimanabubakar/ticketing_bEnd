5a50e48e30c90f462a7098aca85b6aa67ecbc62f
hassanabuya82 <hassanabuya82@gmail.com>
2023-03-13 21:00:13 +0300

    api-browsable

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard_api, name='dashboard_api'),
+    path('', views.dashboard_api, name='dashboard_api'),
 ]
 
 
